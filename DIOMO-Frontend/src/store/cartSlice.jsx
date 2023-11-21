25b0ef39078658e9eb65ce7b82eb2fe6d67e7caa
sanjkthakur18 <131395670+sanjkthakur18@users.noreply.github.com>
2023-11-21 15:13:56 +0530

    Update cartSlice.jsx

--- DIOMO-Frontend/src/store/cartSlice.jsx
+++ DIOMO-Frontend/src/store/cartSlice.jsx
@@ -1,10 +1,8 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { getProductSingle, getSingleProductStatus } from "./productSlice";
-import { STATUS } from '../utils/status';
+import {createSlice} from "@reduxjs/toolkit";
 
 const fetchFromLocalStorage = () => {
     let cart = localStorage.getItem('cart');
-    if (cart) {
+    if(cart){
         return JSON.parse(localStorage.getItem('cart'));
     } else {
         return [];
@@ -27,46 +25,27 @@ const cartSlice = createSlice({
     initialState,
     reducers: {
         addToCart: (state, action) => {
-            const isItemInCart = state.carts.find(
-                (item) => item.productId === action.payload.productId
-            );
+            const isItemInCart = state.carts.find(item => item.id === action.payload.id);
 
-            const singleProductStatus = getSingleProductStatus(state);
+            if(isItemInCart){
+                const tempCart = state.carts.map(item => {
+                    if(item.id === action.payload.id){
+                        let tempQty = item.quantity + action.payload.quantity;
+                        let tempTotalPrice = tempQty * item.price;
 
-            if (singleProductStatus === STATUS.SUCCEEDED) {
-                if (isItemInCart) {
-                    state.carts = state.carts.map((item) => {
-                        if (item.productId === action.payload.productId) {
-                            const tempQty = item.quantity + 1;
-                            const tempTotalPrice = tempQty * item.product.discountedPrice;
-                            return {
-                                ...item,
-                                quantity: tempQty,
-                                totalPrice: tempTotalPrice
-                            };
-                        } else {
-                            return item;
+                        return {
+                            ...item, quantity: tempQty, totalPrice: tempTotalPrice
                         }
-                    });
-                } else {
-                    const newCartItem = {
-                        productId: action.payload.productId,
-                        quantity: 1,
-                        product: {
-                            title: getProductSingle(state).title,
-                            brand: getProductSingle(state).brand,
-                            rating: getProductSingle(state).rating,
-                            category: getProductSingle(state).category,
-                            price: getProductSingle(state).price,
-                            discountedPrice: getProductSingle(state).discountedPrice,
-                        },
-                        cartTotal: getProductSingle(state).discountedPrice,
-                        discountedPrice: getProductSingle(state).discountedPrice,
-                    };
+                    } else {
+                        return item;
+                    }
+                });
 
-                    state.carts.push(newCartItem);
-                    storeInLocalStorage(state.carts);
-                }
+                state.carts = tempCart;
+                storeInLocalStorage(state.carts);
+            } else {
+                state.carts.push(action.payload);
+                storeInLocalStorage(state.carts);
             }
         },
 
@@ -91,23 +70,23 @@ const cartSlice = createSlice({
 
         toggleCartQty: (state, action) => {
             const tempCart = state.carts.map(item => {
-                if (item.id === action.payload.id) {
+                if(item.id === action.payload.id){
                     let tempQty = item.quantity;
                     let tempTotalPrice = item.totalPrice;
 
-                    if (action.payload.type === "INC") {
+                    if(action.payload.type === "INC"){
                         tempQty++;
-                        if (tempQty === item.stock) tempQty = item.stock;
+                        if(tempQty === item.stock) tempQty = item.stock;
                         tempTotalPrice = tempQty * item.discountedPrice;
                     }
 
-                    if (action.payload.type === "DEC") {
+                    if(action.payload.type === "DEC"){
                         tempQty--;
-                        if (tempQty < 1) tempQty = 1;
+                        if(tempQty < 1) tempQty = 1;
                         tempTotalPrice = tempQty * item.discountedPrice;
                     }
 
-                    return { ...item, quantity: tempQty, totalPrice: tempTotalPrice };
+                    return {...item, quantity: tempQty, totalPrice: tempTotalPrice};
                 } else {
                     return item;
                 }
@@ -127,7 +106,7 @@ const cartSlice = createSlice({
     }
 });
 
-export const { addToCart, setCartMessageOff, setCartMessageOn, getCartTotal, toggleCartQty, clearCart, removeFromCart } = cartSlice.actions;
+export const {addToCart, setCartMessageOff, setCartMessageOn, getCartTotal, toggleCartQty, clearCart, removeFromCart} = cartSlice.actions;
 export const getAllCarts = (state) => state.cart.carts;
 export const getCartItemsCount = (state) => state.cart.itemsCount;
 export const getCartMessageStatus = (state) => state.cart.isCartMessageOn;
